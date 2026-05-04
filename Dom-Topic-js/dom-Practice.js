681521268cd137936656ec06860f3ba2562911a1
Jaswinder Singh <jaswindersingh46042@gmail.com>
2026-05-04 08:21:21 +0530

    JavaScript topic Practice  276-04-26

---
+++ Dom-Topic-js/dom-Practice.js
@@ -0,0 +1,12 @@
+// DOM is one of the most imortant JavaScript topics because it connects JavaScript with Html/Css.
+
+
+//  📌 DOM Selection Methods
+
+// 1️⃣ getElementById()
+
+let data = document.getElementById("title")
+console.log(data);
+
+
+//  
